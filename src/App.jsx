cf0ffd397e89d700a3e11491401ea07aa194c679
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-03-15 22:11:19 +0500

    done with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Signup from "./components/pages/Signup";
+import Products from "./components/pages/Products";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  let navigate = useNavigate();
   return (
     <>
-      <h1>Hello From Navbar</h1>
+      <nav>
+        <div className="container">
+          <div className="logo">
+            <h1>TRVL</h1>
+            <i className="fas fa-rocket"></i>
+          </div>
+          <div className="links">
+            <ul>
+              <li onClick={() => navigate("/home")}>Home</li>
+              <li onClick={() => navigate("/services")}>Services</li>
+              <li onClick={() => navigate("/products")}>Products</li>
+              <li onClick={() => navigate("/signup")}>
+                <Button style={{border:'1px solid white',color:'lightgrey',padding:'0.5rem 1rem',fontWeight:'bold',fontSize:'1rem'}}>SIGN UP</Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
