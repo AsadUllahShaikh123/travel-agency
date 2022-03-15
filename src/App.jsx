4118b73f69aaf3d07416ebd2ed0ba0f3e2514448
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-03-15 19:31:19 +0500

    Added Navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <h1>Hello From App Again without Again without</h1>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
